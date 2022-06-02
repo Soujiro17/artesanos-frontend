cd16e4e399d9b67c1300cd700f22a5084535c7f7
Soujiro17 <vicente_tomi@hotmail.com>
2022-06-02 13:20:48 -0400

    modal created

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Modal = ({ children, ...props }) => {
+  return ReactDOM.createPortal(
+    <div>{children}</div>,
+    document.getElementById("modal")
+  )
+}
+
+export default Modal
