bd8939dc9516d8260a255319224b22df24eb8b28
Soujiro17 <vicente_tomi@hotmail.com>
2022-07-02 13:44:17 -0400

    pagina artesano creada

--- src/pages/artesano/index.jsx
+++ src/pages/artesano/index.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { useQuery } from 'react-query'
 import { Link, useParams } from 'react-router-dom'
-import { Layout, Section, Spinner, StackCircles } from '../../components'
+import { Layout, OrangeLine, Section, Spinner, StackCircles } from '../../components'
 import useApi from '../../hooks/useApi'
 import styles from './styles.module.scss'
+
+const FotoConAnchorYText = ({ value = {}, url }) => {
+  return (
+    <Link to={url || '#'}>
+      <div className={styles.producto_foto_group}>
+        <img className={`${styles.foto}`} src={value.picture_url} />
+        <p className={styles.producto_nombre}>{value.nombre}</p>
+      </div>
+    </Link>
+  )
+}
 
 const Artesano = () => {
   const params = useParams()
@@ -12,8 +23,6 @@ const Artesano = () => {
 
   const { data: artesano, isLoading } = useQuery(['artesano', params.id], () => api.getArtesanoById({ _id: params.id }))
   const { data: pymes, isLoading: isLoadingPymes } = useQuery(['pymes', params.id], () => api.getArtesanoPymes({ _id: params.id }))
-
-  console.log(artesano, pymes)
 
   return (
     <Layout>
@@ -26,42 +35,37 @@ const Artesano = () => {
               <p className={`${styles.artesano_nombre} color-p`}>{artesano?.nombres} <span className={`${styles.artesano_apellido} bb-gc`}>{artesano?.apellidos}</span></p>
             </div>
             <div className={styles.artesano_datos}>
-              <div className={styles.artesanos_fotos}>
-                <div className={styles.artesano_pymes}>
-                  {
-                    pymes?.slice(0, 2).map(pyme => (
-                      <Link to='#' key={pyme._id}>
-                        <div className={styles.producto_foto_group}>
-                          <img className={`${styles.foto}`} src={pyme.picture_url} />
-                          <p className={styles.producto_nombre}>{pyme.nombre}</p>
-                        </div>
-                      </Link>
-                    ))
+              <div className={styles.artesano_datos_cont}>
+                <div className={styles.artesanos_fotos}>
+                  <div className={styles.artesano_pymes}>
+                    <p className={`${styles.text} color-p`}>Pymes</p>
+                    {
+                    pymes?.slice(0, 2).map(pyme => <FotoConAnchorYText value={pyme} key={pyme._id} />)
                   }
+                  </div>
+                  <div className={styles.artesano_foto}>
+                    <p className={`${styles.text} color-p`}>Artesano</p>
+                    <img src={artesano?.picture_url} className={`${styles.artesanofoto}`} />
+                  </div>
                 </div>
-                <div className={styles.artesano_foto}>
-                  <img src={artesano?.picture_url} className={`${styles.artesanofoto}`} />
+                <div className={styles.artesano_productos}>
+                  <p className={`${styles.text} color-p`}>Productos</p>
+                  <div className={styles.artesano_productos_cont}>
+                    <FotoConAnchorYText value={{ picture_url: artesano?.picture_url, nombre: 'Text' }} />
+                    <FotoConAnchorYText value={{ picture_url: artesano?.picture_url, nombre: 'Text' }} />
+                    <FotoConAnchorYText value={{ picture_url: artesano?.picture_url, nombre: 'Text' }} />
+                  </div>
+                  <Link to='#' className={styles.anchor_btn}>
+                    <button className='bg-cyan btn-effect btn'>Más productos</button>
+                  </Link>
                 </div>
               </div>
-              <div className={styles.artesano_productos}>
-                <Link to='#'>
-                  <div className={styles.producto_foto_group}>
-                    <img src={artesano?.picture_url} className={`${styles.foto}`} />
-                    <p className={styles.producto_nombre}>Text</p>
-                  </div>
-                </Link>
-                <Link to='#'>
-                  <div className={styles.producto_foto_group}>
-                    <img src={artesano?.picture_url} className={`${styles.foto}`} />
-                    <p className={styles.producto_nombre}>Text</p>
-                  </div>
-                </Link>
-                <Link to='#'>
-                  <div className={styles.producto_foto_group}>
-                    <img src={artesano?.picture_url} className={`${styles.foto}`} />
-                    <p className={styles.producto_nombre}>Text</p>
-                  </div>
-                </Link>
+              <OrangeLine />
+              <div className={styles.sobre_emprendimiento}>
+                <p className={styles.emprendimiento_title}>Sobre el emprendimiento</p>
+                <p className={styles.emprendimiento_text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
               </div>
             </div>
           </div>
