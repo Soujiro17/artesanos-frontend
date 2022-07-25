ebcac4e1f29c5fd48d93489ada83da507a4b3ad1
Soujiro17 <vicente_tomi@hotmail.com>
2022-07-25 00:49:29 -0400

    descripcion artesano hecha

--- src/pages/artesano/index.jsx
+++ src/pages/artesano/index.jsx
@@ -104,7 +104,7 @@ const Artesano = () => {
                 <div className={styles.sobre_emprendimiento}>
                   <p className={styles.emprendimiento_title}>Sobre el emprendimiento</p>
                   <p className={styles.emprendimiento_text}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    {pymes[0]?.descripcion}
                   </p>
                 </div>
                 <ArtesanoMapa pymes={pymes} />
