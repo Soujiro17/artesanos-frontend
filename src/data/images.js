3e058ea8bafe758ed34d40f048319a8afea4cdbc
Soujiro17 <vicente_tomi@hotmail.com>
2022-10-16 16:51:37 -0300

    imágenes subidas a cloudinary

--- src/data/images.js
+++ src/data/images.js
@@ -1,12 +1,12 @@
 export const imgs = {
-  logo: '/logo.png',
+  logo: 'https://res.cloudinary.com/artesanos/image/upload/v1665949086/logo_b1bgcd.png',
   presentacion: '',
-  buscar_categoria: '/home_circles.png',
-  acerca_de_nosotros: '/san_miguel.jpg',
-  acerca_de_nosotros_circulo_azul: '/circulo_azul.png',
-  acerca_de_nosotros_circulo_rayado: '/circulo_rayas.png',
-  artesano_no_encontrado: '/img/artesano_no_encontrado.png',
-  categoria_no_encontrada: '/img/categoria_no_encontrada.png',
-  producto_no_encontrado: '/img/producto_no_encontrado.jpg',
-  no_encontrado: '/img/not_found_default.jpg'
+  buscar_categoria: 'https://res.cloudinary.com/artesanos/image/upload/v1665949089/home_circles_wh2msj.png',
+  acerca_de_nosotros: 'https://res.cloudinary.com/artesanos/image/upload/v1665949086/san_miguel_ulozw2.jpg',
+  acerca_de_nosotros_circulo_azul: 'https://res.cloudinary.com/artesanos/image/upload/v1665949086/circulo_azul_iqoi5g.png',
+  acerca_de_nosotros_circulo_rayado: 'https://res.cloudinary.com/artesanos/image/upload/v1665949086/circulo_rayas_ugr3lv.png',
+  artesano_no_encontrado: 'https://res.cloudinary.com/artesanos/image/upload/v1665949330/artesano_no_encontrado_kjdywg.png',
+  categoria_no_encontrada: 'https://res.cloudinary.com/artesanos/image/upload/v1665949330/categoria_no_encontrada_rvffl6.png',
+  producto_no_encontrado: 'https://res.cloudinary.com/artesanos/image/upload/v1665949330/producto_no_encontrado_cqlkir.jpg',
+  no_encontrado: 'https://res.cloudinary.com/artesanos/image/upload/v1665949330/not_found_default_od4jx9.jpg'
 }
