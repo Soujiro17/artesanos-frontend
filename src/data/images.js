2582a5949715150ec94c23c48af72a6f06555825
Soujiro17 <vicente_tomi@hotmail.com>
2022-10-26 17:28:46 -0300

    última foto agregada

--- src/data/images.js
+++ src/data/images.js
@@ -1,6 +1,6 @@
 export const imgs = {
   logo: 'https://res.cloudinary.com/artesanos/image/upload/v1665949086/logo_b1bgcd.png',
-  presentacion: '',
+  presentacion: 'https://res.cloudinary.com/artesanos/image/upload/v1666816036/287864659_5328984917194834_7461702740001004505_n_skhnfg.jpg',
   buscar_categoria: 'https://res.cloudinary.com/artesanos/image/upload/v1665949089/home_circles_wh2msj.png',
   acerca_de_nosotros: 'https://res.cloudinary.com/artesanos/image/upload/v1665949086/san_miguel_ulozw2.jpg',
   acerca_de_nosotros_circulo_azul: 'https://res.cloudinary.com/artesanos/image/upload/v1665949086/circulo_azul_iqoi5g.png',
